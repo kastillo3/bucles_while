4f92ecb9a5fbc65cd67248d30ae9b4cb349f36c0
ism47597075 <ism47597075@a06.informatica.escoladeltreball.org>
2018-01-29 13:28:31 +0100

    omg

---
+++ mayorK01.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+numero1=input("introduce un numero")
+numero2=input("introduce un numero")
+while (numero1>numero2):
+	numero2=input("intentalo de nuevo")
+print("es mayor")
---
+++ mayorK02.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+numero1=float(input("introduce un numero"))
+numero2=input("introduce un numero")
+while (numero2>numero1):
+	numero2=input("intentalo de nuevo")
+print("es mayor")
