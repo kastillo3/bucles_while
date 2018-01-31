98561cc990dfb6eccd1e381c7214348c7646a861
ism47597075 <ism47597075@a06.informatica.escoladeltreball.org>
2018-01-31 12:15:49 +0100

    hola

---
+++ mayorK05.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+numero1=input("introduce un numero")
+while (numero1>-1):
+	numero2=input(":")
---
+++ nveces01.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+x=input("introduce un numero")
+i = 0
+while (i<x):
+    i+=1 
+print(input("introduce un numero"))
+
