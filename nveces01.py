a12fe41ebe73fb356ed81fecf4aa91c68d2fcda9
ism47597075 <ism47597075@a06.informatica.escoladeltreball.org>
2018-02-01 12:59:33 +0100

    omg

--- nveces01.py
+++ nveces01.py
@@ -1,7 +1,10 @@
-# coding: utf-8
+#coding: utf-8
 x=input("introduce un numero")
-i = 0
-while (i<x):
-    i+=1 
-print(input("introduce un numero"))
-
+cont=0
+suma=0
+while (cont<x):
+	aux=input("introduce otro numero")
+	if aux>0:
+		cont+=1
+		suma=suma+aux
+print(suma)
