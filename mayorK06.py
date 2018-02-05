ede4b8c1fa2592d62f1be8162cae2fdf6bf5fe0c
kastillo3 <35691382+kastillo3@users.noreply.github.com>
2018-02-05 12:42:05 +0100

    Update mayorK06.py

--- mayorK06.py
+++ mayorK06.py
@@ -1,15 +1,8 @@
 #coding: utf-8
 x=float(input("introduce un numero:"))
 cont=0
-suma=0
-while (x<=0):
-    x=float(input("Error, introduce otro:"))
-while (x>cont): 
-    aux=float(input("introduce otro numero:"))
-    if aux>0:
-        cont+=x
-        suma2=suma+aux
-        if suma2>=x:
-            print "Programa terminado"
-
+while (cont<=x):
+	aux=input("introduce otro numero:")
+	cont=cont+aux
+print cont
 
