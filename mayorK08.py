419dd30c00eacc937bd78687155e593f3a744bb5
kastillo3 <35691382+kastillo3@users.noreply.github.com>
2018-02-06 18:27:16 +0100

    Create mayorK08.py

---
+++ mayorK08.py
@@ -0,0 +1,21 @@
+numero = int(input("Escriba un número par: "))
+while numero %2!= 0:
+    numero = int(input(str(numero) + " no es un número par. Inténtelo de nuevo: "))
+contador = 1
+r = input("¿Quiere escribir otro número par? (SI/NO) ")
+
+while r == "SI":
+    numero = int(input("Escriba un número par: "))
+    while numero %2!= 0:
+        numero = int(input(str(numero) + " no es un número par. Inténtelo de nuevo: "))
+    contador += 1
+    r = input("¿Quiere escribir otro número par? (SI/NO) ")
+
+print()
+if contador == 0:
+    print("No ha escrito ningún número par")
+elif contador == 1:
+    print("Ha escrito 1 número par")
+else:
+    print("Ha escrito", contador, "números pares.")
+print("Programa terminado")
