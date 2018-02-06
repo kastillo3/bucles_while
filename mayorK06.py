6e559ffbf88bb2489d77b338377ca94a20a220dd
kastillo3 <35691382+kastillo3@users.noreply.github.com>
2018-02-06 18:17:04 +0100

    Update mayorK06.py

--- mayorK06.py
+++ mayorK06.py
@@ -1,8 +1,14 @@
 #coding: utf-8
-x=float(input("introduce un numero:"))
-cont=0
-while (cont<=x):
-	aux=input("introduce otro numero:")
-	cont=cont+aux
-print cont
+x = float(input("Escriba el valor límite: "))
+while x <= 0:
+    x = float(input("El límite debe ser mayor que 0. Inténtelo de nuevo: "))
+
+numero = float(input("Escriba un número: "))
+suma = 0
+suma += numero
+
+while suma < x:
+    numero = float(input("Escriba otro número: "))
+    suma += numero
+print("Programa terminado")
 
