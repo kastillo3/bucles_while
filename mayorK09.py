5b1d77b286428aed736478b7133fa40d2e6a7e04
kastillo3 <35691382+kastillo3@users.noreply.github.com>
2018-02-07 11:37:36 +0100

    Update mayorK09.py

--- mayorK09.py
+++ mayorK09.py
@@ -1,11 +1,11 @@
 n = int(input("Escriba un número entero mayor que 1: "))
 while n <= 1:
     n = int(input(str(n) + " no es mayor que 1. Inténtelo de nuevo: "))
-copia = n
+c = n
 
 print("numeros primos: ", final="")
 for i in range(2, n + 1):
-    while copia % i == 0:
-        copia = copia // i
+    while c % i == 0:
+        c = c // i
         print(i, final=" ")
 print("Programa terminado")
